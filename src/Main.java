5eec01b102c6a94fed378793c2e0c9d213c9b581
Faraz <farazchahi2079@gmail.com>
2023-12-27 21:14:23 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public void rethrowException(int x) throws IOException {
+        if (x < 0) {
+            throw new IOException("Input cannot be negative");
+        }
+        try {
+            System.out.println("only positive");
+            //we can also use some certain condition here for throwing
+        } catch (Exception e) {
+            e.printStackTrace(); // Print the stack trace
+            throw new RuntimeException("Error occurred", e); // Chain the exception
+        }
+    }
+}
